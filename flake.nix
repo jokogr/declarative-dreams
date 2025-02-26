9bd98e405c4e60fa56709ed30da37de25dd7ed3b
Ioannis Koutras <ioannis.koutras@gmail.com>
2025-02-27 00:10:15 +0100

    feat: initial setup for vm

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.disko.url = "github:nix-community/disko";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+
+  outputs =
+    {
+      nixpkgs,
+      disko,
+      nixos-facter-modules,
+      ...
+    }:
+    {
+      nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          (
+            { pkgs, ... }:
+            {
+              imports = [
+                ./disk-config.nix
+              ];
+
+              boot.loader.systemd-boot.enable = true;
+
+              environment.systemPackages = map pkgs.lib.lowPrio [
+                pkgs.curl
+                pkgs.gitMinimal
+              ];
+
+              services.openssh.enable = true;
+
+              users.users.root.openssh.authorizedKeys.keyFiles = [
+                ./id_rsa.pub
+              ];
+
+              system.stateVersion = "24.11";
+            }
+          )
+          nixos-facter-modules.nixosModules.facter
+          {
+            config.facter.reportPath =
+              if builtins.pathExists ./facter.json then
+                ./facter.json
+              else
+                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+          }
+        ];
+      };
+    };
+}
